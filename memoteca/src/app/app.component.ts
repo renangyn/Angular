c36223f32e53af66a289b5a5efbc58fea9ad34af
Renan <teste>
2025-01-29 15:29:52 -0300

    c

---
+++ memoteca/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'memoteca';
+}
